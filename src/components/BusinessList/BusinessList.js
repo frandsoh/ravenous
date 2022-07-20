a4f8497f36eb2941b3803998a0ec48084a5fbcf2
Frands Otting <frands@otting.dk>
2022-07-20 09:08:17 +0200

    export BusinessList component

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,4 +2,16 @@ import React from 'react'
 import "./BusinessList.css"
 import Business from '../Business/Business';
 
-
+class BusinessList extends React.Component {
+  render() {
+    <div className="BusinessList">
+      <Business />
+      <Business />
+      <Business />
+      <Business />
+      <Business />
+      <Business />
+    </div>
+  }
+}
+export default BusinessList;
