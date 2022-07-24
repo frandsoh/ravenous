f893c504132323cd2ba222fafebfddeca2187571
Frands Otting <frands@otting.dk>
2022-07-24 20:48:37 +0200

    Add app constructor

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,23 @@ import React from "react";
 import "./App.css";
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from "../SearchBar/SearchBar"
-
+import Yelp from "../../util/Yelp"
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy) {
-   console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`)   
+    Yelp.search(term, location, sortBy)
+      .then(businesses => {
+        this.setState({ businesses: businesses })
+      })
   }
 
 
@@ -18,7 +29,7 @@ class App extends React.Component {
 
         <SearchBar searchYelp={this.searchYelp} />
 
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
